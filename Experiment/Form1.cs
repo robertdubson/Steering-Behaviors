0957bc844b5654353f7de646ff7be6b2730df026
robertdubson <dubson2001@gmail.com>
2021-12-06 04:41:54 +0200

    Expiremt with runaway behavior as inversion of seek behavior.

--- Experiment/Form1.cs
+++ Experiment/Form1.cs
@@ -30,7 +30,7 @@ namespace Experiment
 
             button.Width = button.Height;
 
-            button.Location = new Point(1, 1);
+            button.Location = new Point(500, 500);
 
             button.BackColor = Color.Blue;
 
@@ -47,22 +47,27 @@ namespace Experiment
 
         private void GameLoop() {
 
-            
-            
-            
             if (Boid != null) {
 
                 //Boid.Location = Cursor.Position;
 
                 float radius = 3;
 
-                float maxspeed = 3;
+                float maxspeed = 2;
 
                 float maxforce = 0.1f;
 
                 Vector location = new Vector(Boid.Location.X, Boid.Location.Y);
 
-                Vector target = new Vector(Cursor.Position.X, Cursor.Position.Y);
+                float circleLength = radius * 2 * 3.1415f;
+
+                Random rand = new Random();
+
+                Point heighPointOfACircle = new Point(Boid.Location.X, (int)(Boid.Location.Y + radius));
+
+                Point randomPoint = new Point();
+
+                Vector target = new Vector((int)randomPoint.X, (int)randomPoint.Y);
 
                 Vector desired = Vector.Subtract(target, location);
 
@@ -87,6 +92,80 @@ namespace Experiment
             }
             
         
+        }
+
+        private void gathering() {
+        
+
+        
+        }
+
+        private void runaway() {
+
+            float radius = 3;
+
+            float maxspeed = 2;
+
+            float maxforce = 0.1f;
+
+            Vector location = new Vector(Boid.Location.X, Boid.Location.Y);
+
+            Vector target = new Vector(Cursor.Position.X, Cursor.Position.Y);
+
+            Vector desired = Vector.Subtract(target, location) * -1;
+
+            desired.Normalize();
+
+            desired = desired * maxspeed;
+
+            Vector steer = Vector.Subtract(desired, velocity);
+
+            acceleration = acceleration + steer;
+
+            velocity = velocity + acceleration;
+
+            //velocity.limit(maxspeed);
+
+            location = location + velocity;
+
+            Boid.Location = new Point((int)location.X, (int)location.Y);
+
+            acceleration = acceleration * 0;
+
+        }
+
+        private void seek() {
+
+            float radius = 3;
+
+            float maxspeed = 3;
+
+            float maxforce = 0.1f;
+
+            Vector location = new Vector(Boid.Location.X, Boid.Location.Y);
+
+            Vector target = new Vector(Cursor.Position.X, Cursor.Position.Y);
+
+            Vector desired = Vector.Subtract(target, location);
+
+            desired.Normalize();
+
+            desired = desired * maxspeed;
+
+            Vector steer = Vector.Subtract(desired, velocity);
+
+            acceleration = acceleration + steer;
+
+            velocity = velocity + acceleration;
+
+            //velocity.limit(maxspeed);
+
+            location = location + velocity;
+
+            Boid.Location = new Point((int)location.X, (int)location.Y);
+
+            acceleration = acceleration * 0;
+
         }
 
         private void gameTimer_Tick(object sender, EventArgs e)
