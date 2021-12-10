8398946e19a8c8a624cde9ac0bb010f84c13ddef
robertdubson <dubson2001@gmail.com>
2021-12-10 08:59:05 +0200

    Successful gathering experiment.

--- Experiment/Form1.Designer.cs
+++ Experiment/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.components = new System.ComponentModel.Container();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.labelPosition = new System.Windows.Forms.Label();
+            this.labelPointEquivalent = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // gameTimer
@@ -38,20 +40,43 @@
             this.gameTimer.Interval = 1;
             this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
             // 
+            // labelPosition
+            // 
+            this.labelPosition.AutoSize = true;
+            this.labelPosition.Location = new System.Drawing.Point(883, 9);
+            this.labelPosition.Name = "labelPosition";
+            this.labelPosition.Size = new System.Drawing.Size(35, 13);
+            this.labelPosition.TabIndex = 0;
+            this.labelPosition.Text = "label1";
+            // 
+            // labelPointEquivalent
+            // 
+            this.labelPointEquivalent.AutoSize = true;
+            this.labelPointEquivalent.Location = new System.Drawing.Point(883, 33);
+            this.labelPointEquivalent.Name = "labelPointEquivalent";
+            this.labelPointEquivalent.Size = new System.Drawing.Size(35, 13);
+            this.labelPointEquivalent.TabIndex = 1;
+            this.labelPointEquivalent.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(944, 450);
+            this.Controls.Add(this.labelPointEquivalent);
+            this.Controls.Add(this.labelPosition);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label labelPosition;
+        private System.Windows.Forms.Label labelPointEquivalent;
     }
 }
 
--- Experiment/Form1.cs
+++ Experiment/Form1.cs
@@ -12,6 +12,8 @@ using Point = System.Drawing.Point;
 
 namespace Experiment
 {
+    
+
     public partial class Form1 : Form
     {
         public Button Boid { get; set; }
@@ -20,13 +22,66 @@ namespace Experiment
 
         Vector acceleration;
 
+        GameField Field { get; set; }
+
+        List<Animal> animals;
+
         public Form1()
         {
             InitializeComponent();
 
+            this.Height = 768;
+
+            this.Width = 1366;
+
+            //Field = new GameField(Height, Width);
+
+            //Button button = new Button();
+
+            //button.Height = 20;
+
+            //button.Width = button.Height;
+
+            //Random rand = new Random();
+
+            //int r1 = rand.Next(Field.Row);
+
+            //int r2 = rand.Next(Field.Column);
+
+            //button.Location = new Point(r2 * Field.Resolution, r1 * Field.Resolution);
+
+            //.BackColor = Color.Blue;
+
+            //this.Controls.Add(button);
+
+            //Boid = button;
+
+            velocity = new Vector(0, 0);
+
+            acceleration = new Vector(0, 0);
+
+            //Click += Form1_Click;
+
+            animals = new List<Animal>();
+
+            animals.Add(new Animal(new Button(), this, 1.15f));
+
+            animals.Add(new Animal(new Button(), this, 1.0000000000015f));
+
+            //GenerateBoid();
+            
+        }
+
+        //private void Form1_Click(object sender, EventArgs e)
+        //{
+           // Boid.Location = new Point(Cursor.Position.X, Cursor.Position.Y);
+        //}
+
+        private void GenerateBoid() {
+
             Button button = new Button();
 
-            button.Height = 50;
+            button.Height = 40;
 
             button.Width = button.Height;
 
@@ -38,79 +93,90 @@ namespace Experiment
 
             Boid = button;
 
-            velocity = new Vector(0, 0);
 
-            acceleration = new Vector(0, 0);
-
-            
-        }
+        } 
 
         private void GameLoop() {
 
-            if (Boid != null) {
 
-                //Boid.Location = Cursor.Position;
+            foreach (Animal a in animals) {
+
+                gathering(a);               
+            
+            }
+
+            //seek(animals.First(), animals.Last());
+            
+            //gathering(new Animal(new Button(), this, 1.23f));
+ 
+        }
+
+        private void gathering(Animal a) {
 
                 float radius = 3;
 
-                float maxspeed = 2;
+                float maxspeed = a.MaxSpeed;
 
                 float maxforce = 0.1f;
 
-                Vector location = new Vector(Boid.Location.X, Boid.Location.Y);
+                Vector location = new Vector(a.Actor.Location.X, a.Actor.Location.Y);
 
-                float circleLength = radius * 2 * 3.1415f;
+                //Vector target = new Vector((int)randomPoint.X, (int)randomPoint.Y);
 
-                Random rand = new Random();
+                //Vector target = new Vector((int)Cursor.Position.X, (int)Cursor.Position.Y);
 
-                Point heighPointOfACircle = new Point(Boid.Location.X, (int)(Boid.Location.Y + radius));
-
-                Point randomPoint = new Point();
-
-                Vector target = new Vector((int)randomPoint.X, (int)randomPoint.Y);
-
-                Vector desired = Vector.Subtract(target, location);
+                Vector desired = a.FieldOfFlow.Lookup(location);
 
                 desired.Normalize();
 
                 desired = desired * maxspeed;
 
-                Vector steer = Vector.Subtract(desired, velocity);
+                Vector steer = Vector.Subtract(desired, a.Velocity);
 
-                acceleration = acceleration + steer;
+                acceleration = a.Acceleration + steer;
 
-                velocity = velocity + acceleration;
-                
+                a.Velocity = a.Velocity + acceleration;
+
                 //velocity.limit(maxspeed);
-                
-                location = location + velocity;
 
-                Boid.Location = new Point((int)location.X, (int)location.Y);
-                
+                location = location + a.Velocity;
+
+                a.Actor.Location = new Point((int)location.X, (int)location.Y);
+
+                //labelPosition.Text = "( " + Boid.Location.X + ", " + Boid.Location.Y + " )";
+
+                //Point curPoint = new Point(Boid.Location.X, Boid.Location.Y);
+
+                //labelPointEquivalent.Text = "( " + curPoint.X + ", " + curPoint.Y + " )";
+
                 acceleration = acceleration * 0;
 
-            }
-            
+                
+                //Boid.Location = Cursor.Position;
+
+                
+
+
+        }
+
+        private void coupling() 
+        { 
         
         }
 
-        private void gathering() {
-        
 
-        
-        }
 
-        private void runaway() {
+        private void runaway(Animal runner, Animal pursuer) {
 
             float radius = 3;
 
-            float maxspeed = 2;
+            float maxspeed = runner.MaxSpeed;
 
             float maxforce = 0.1f;
 
-            Vector location = new Vector(Boid.Location.X, Boid.Location.Y);
+            Vector location = new Vector(runner.Actor.Location.X, runner.Actor.Location.Y);
 
-            Vector target = new Vector(Cursor.Position.X, Cursor.Position.Y);
+            Vector target = new Vector(pursuer.Actor.Location.X, pursuer.Actor.Location.Y);
 
             Vector desired = Vector.Subtract(target, location) * -1;
 
@@ -118,33 +184,33 @@ namespace Experiment
 
             desired = desired * maxspeed;
 
-            Vector steer = Vector.Subtract(desired, velocity);
+            Vector steer = Vector.Subtract(desired, runner.Velocity);
 
-            acceleration = acceleration + steer;
+            runner.Acceleration = runner.Acceleration + steer;
 
-            velocity = velocity + acceleration;
+            runner.Velocity = runner.Velocity + runner.Acceleration;
 
             //velocity.limit(maxspeed);
 
-            location = location + velocity;
+            location = location + runner.Velocity;
 
-            Boid.Location = new Point((int)location.X, (int)location.Y);
+            runner.Actor.Location = new Point((int)location.X, (int)location.Y);
 
-            acceleration = acceleration * 0;
+            runner.Acceleration = runner.Acceleration * 0;
 
         }
 
-        private void seek() {
+        private void seek(Animal seeker, Animal targetAnimal) {
 
             float radius = 3;
 
-            float maxspeed = 3;
+            float maxspeed = seeker.MaxSpeed;
 
             float maxforce = 0.1f;
 
-            Vector location = new Vector(Boid.Location.X, Boid.Location.Y);
+            Vector location = new Vector(seeker.Actor.Location.X, seeker.Actor.Location.Y);
 
-            Vector target = new Vector(Cursor.Position.X, Cursor.Position.Y);
+            Vector target = new Vector(targetAnimal.Actor.Location.X, targetAnimal.Actor.Location.Y);
 
             Vector desired = Vector.Subtract(target, location);
 
@@ -152,25 +218,277 @@ namespace Experiment
 
             desired = desired * maxspeed;
 
-            Vector steer = Vector.Subtract(desired, velocity);
+            Vector steer = Vector.Subtract(desired, seeker.Velocity);
 
-            acceleration = acceleration + steer;
+            seeker.Acceleration = seeker.Acceleration + steer;
 
-            velocity = velocity + acceleration;
+            seeker.Velocity = seeker.Velocity + seeker.Acceleration;
 
             //velocity.limit(maxspeed);
 
-            location = location + velocity;
+            location = location + seeker.Velocity;
 
-            Boid.Location = new Point((int)location.X, (int)location.Y);
+            seeker.Actor.Location = new Point((int)location.X, (int)location.Y);
 
-            acceleration = acceleration * 0;
+            seeker.Acceleration = seeker.Acceleration * 0;
 
         }
 
         private void gameTimer_Tick(object sender, EventArgs e)
         {
             GameLoop();
+
         }
+
+        private void Rearrenge() {
+
+            Field = new GameField(Height, Width);
+        
+        }
+
+    }
+
+    public class GameField
+    {
+
+        public Vector[,] FieldArray { get; set; }
+
+        public int Resolution { get; set; }
+
+        public int Row { get; }
+
+        public int Column { get; }
+
+        public void Rearreange() {
+
+            int[] directions = new int[3];
+
+            directions[0] = -1;
+
+            directions[1] = 0;
+
+            directions[2] = 1;
+
+
+            for (int i = 0; i < Row; i++) 
+            {
+                for (int j = 0; j < Column; j++) {
+
+                    Random rand = new Random();
+
+                    int r1 = rand.Next(3);
+
+                    int r2 = rand.Next(3);
+
+                    FieldArray[i, j].X = directions[r1];
+
+                    FieldArray[i, j].Y = directions[r2];
+
+
+                }
+            
+            
+            
+            }
+
+
+        }
+
+        public GameField(int Height, int Width)
+        {
+            Resolution = 10;
+
+            int row = Height / Resolution;
+
+            int column = Width / Resolution;
+
+            Row = row;
+
+            Column = column;
+
+            FieldArray = new Vector[row, column];
+
+            int[] directions = new int[3];
+
+            directions[0] = -1;
+
+            directions[1] = 0;
+
+            directions[2] = 1;
+
+            //for (int i = 0; i < column; i++) 
+            //{
+
+             //   FieldArray[0, 1] = new Vector(1, 0);
+
+           // }
+            
+            for (int i = 0; i < row; i++) 
+            {
+
+                //FieldArray[i, 0] = new Vector(0, 1); 
+
+                for (int j =0; j< column; j++) 
+                {
+                    Random random = new Random();
+
+                    int r1 = random.Next(3);
+
+                    int r2 = random.Next(3);
+
+                    FieldArray[i, j] = new Vector(directions[r1], directions[r2]);
+
+                    /*FieldArray[i, j] = new Vector(-1, 0);
+
+                    if (j % 8 == 0 || j % 17 == 0 || j % 13 == 0)
+                    {
+
+                        FieldArray[i, j] = new Vector(1, 0);
+
+                    }
+                    else if (j % 6 == 0 || j % 37 ==0 || j % 41==0)
+                    {
+
+                        FieldArray[i, j] = new Vector(0, 1);
+
+                    }
+                    else if (j % 5 == 0  || j % 23==0 || j % 29==0)
+                    {
+
+                        FieldArray[i, j] = new Vector(1, 0);
+
+                    }
+                    else if (j % 7 == 0)
+                    {
+
+                        FieldArray[i, j] = new Vector(-1, 0);
+
+                    }
+                    else if (j % 11 == 0)
+                    {
+
+                        FieldArray[i, j] = new Vector(0, -1);
+
+                    }
+                    else if (j % 3 == 0) {
+                        
+                        FieldArray[i, j] = new Vector(-1, -1);
+                    
+                    }*/
+
+                
+                }
+
+            
+            }
+
+
+
+        }
+
+        public Vector Lookup(Vector lookup)
+        {
+
+            int row = (int)lookup.Y / Resolution;
+
+            int column = (int)lookup.X / Resolution;
+
+            /*if (row >= Row) {
+
+                while (row >= Row) {
+
+                    row -= 1;
+
+                }
+            
+            }
+            if (column >= Column) {
+
+                while (column>= Column) {
+
+                    column -= 1;
+                
+                }
+ 
+            }
+            if (row < 0) {
+
+                while (row < 0)
+                {
+
+                    row += 1;
+
+                }
+
+            }
+
+            if (column < 0) {
+
+                while (column < 0)
+                {
+
+                    column += 1;
+
+                }
+
+            }*/
+
+            Random r1 = new Random();
+
+            row = r1.Next(this.Row - 1);
+
+            column = r1.Next(this.Column - 1);
+
+            return new Vector(FieldArray[row, column].X, FieldArray[row, column].Y);
+
+        }
+
+
+
+    }
+
+    public class Animal {
+    
+        public Button Actor { get; set; }
+
+        public GameField FieldOfFlow { get; set; }
+
+        public Vector Acceleration { get; set; }
+
+        public Vector Velocity { get; set; } 
+
+        public float MaxSpeed { get; set; }
+
+        public Animal(Button actor, Form1 form, float maxspeed)
+        {
+            Actor = actor;
+
+            FieldOfFlow = new GameField(form.Height, form.Width);
+
+            Acceleration = new Vector(0, 0);
+
+            Velocity = new Vector(0, 0);
+
+            MaxSpeed = maxspeed;
+
+            Actor.Height = 20;
+
+            Actor.Width = Actor.Height;
+
+            Random rand = new Random();
+
+            int r1 = rand.Next(FieldOfFlow.Row);
+
+            int r2 = rand.Next(FieldOfFlow.Column);
+
+            Actor.Location = new Point(r2 * FieldOfFlow.Resolution, r1 * FieldOfFlow.Resolution);
+
+            Actor.BackColor = Color.Blue;
+
+            form.Controls.Add(Actor);
+
+        }
+
+    
     }
 }
